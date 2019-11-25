c77867c4661f6facc95a14c4403feaee4cf8e6dd
Varun Peesapati <peesapativarun99@gmail.com>
2019-11-24 17:28:30 -0800

    first commit

---
+++ Assets/Scripts/ExitOnClick.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ExitOnClick : MonoBehaviour
+{
+    public void ExitGame()
+    {
+        // To quit applications use - Application.Quit();.
+        // To quit from the editor use the following.
+        EditorApplication.isPlaying = false;
+    }
+}
