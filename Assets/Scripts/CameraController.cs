516ee915d040fb6788d0fe9e8dadc880e456be8f
sahil <ssfaruque1@gmail.com>
2019-11-29 17:30:04 -0800

    Added scene with a controllable character with basic movements

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = new Vector3(this.player.position.x, this.player.position.y, this.player.position.z - 1);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody2D Rb;
+    [SerializeField]
+    private Animator Anim;
+
+    private bool IsGrounded = true;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.A))
+        {
+            Rb.velocity = new Vector2(-5, Rb.velocity.y);
+            this.transform.localScale = new Vector2(-1, 1);
+            this.Anim.SetBool("Run", true);
+        }
+
+        else if(Input.GetKey(KeyCode.D))
+        {
+            Rb.velocity = new Vector2(5, Rb.velocity.y);
+            this.transform.localScale = new Vector2(1, 1);
+            this.Anim.SetBool("Run", true);
+        }
+
+        else 
+        {
+             this.Anim.SetBool("Run", false);
+        }
+
+        if(Input.GetKey(KeyCode.Space))
+        {
+            Rb.velocity = new Vector2(Rb.velocity.x, 5);
+            this.IsGrounded = false;
+        }
+
+
+    }
+}
