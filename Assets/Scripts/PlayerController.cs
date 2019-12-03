e3f6984d2c18cbc90681cab4ab7cda298636f998
Ian Douglas Scott <ian@iandouglasscott.com>
2019-12-02 16:41:30 -0800

    Freeze the player's rotation
    Otherwise, it's possible to spin off the edge of the platform

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -32,7 +32,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Rb.freezeRotation = true;
     }
 
     // Update is called once per frame
