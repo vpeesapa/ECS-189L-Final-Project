6f94d23bb3c3085dde3654b4c79fea5e1d08e606
Varun Peesapati <peesapativarun99@gmail.com>
2019-12-09 14:07:26 -0800

    Updated movement for the blade in level 2

--- Assets/Scripts/BladeController.cs
+++ Assets/Scripts/BladeController.cs
@@ -15,9 +15,11 @@ public class BladeController : MonoBehaviour
     // 1 to start moving right/up first or -1 to start moving left/down first.
     [SerializeField] private int InitialDirection = -1;
 
-    void Start()
-    {
+    private Vector3 InitialPosition;
 
+    void Awake()
+    {
+        this.InitialPosition = this.transform.position;
     }
 
     void Update()
@@ -35,21 +37,19 @@ public class BladeController : MonoBehaviour
         }
 
         // Linear Translation of the blade.
-        var deltaMovement = (Mathf.Sin(Time.time * this.SawSpeed) * Time.deltaTime * this.MaxDistance) * this.InitialDirection;
-        var transformedPosition = this.transform.position;
-
+        Vector3 position = this.InitialPosition;
+        var offset = (Mathf.Sin(Time.time * this.SawSpeed) + 1) * this.InitialDirection * this.MaxDistance / 2;
         if(MoveHorizontally && !MoveVertically)
         {
-            // If the saw moves horizontally.
-            transformedPosition.x += deltaMovement;
-            this.transform.position = transformedPosition;
+            // If the blade moves horizontally.
+            position.x += offset;
         }
         else if(MoveVertically && !MoveHorizontally)
         {
-            // If the saw moves vertically.
-            transformedPosition.y += deltaMovement;
-            this.transform.position = transformedPosition;
+            // If the blade moves vertically.
+            position.y += offset;
         }
+        this.transform.position = position;
     }
 
 }
